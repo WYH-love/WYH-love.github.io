726ccda72b80fb20d016c8d8c220406ae2076c38
WYH-love <2846663931@qq.com>
2025-06-30 19:17:18 +0800

    Site updated: 2025-06-30 19:17:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/STM32F1/W5500使用/","/STM32F1/hello-world/","2025/06/23/第一个文章/"];function toRandomPost(){
+var posts=["/STM32F1/hello-world/","2025/06/23/第一个文章/","/STM32F1/W5500使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
