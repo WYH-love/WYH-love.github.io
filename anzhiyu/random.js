e96670214e12d94ba529244b05cb28f3e23ae31a
WYH-love <2846663931@qq.com>
2025-07-03 16:49:27 +0800

    Site updated: 2025-07-03 16:49:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/STM32F1/W5500使用/","2025/07/01/Hexo/图床教程1/","2025/07/02/Hexo/图床教程2/","2025/07/01/Hexo/图床教程/"];function toRandomPost(){
+var posts=["W5500使用/","Hexo/图床教程/","Hexo/图床教程1/","Hexo/图床教程2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
