284692a7de7269936783dee79281601c0023cbef
WYH-love <2846663931@qq.com>
2025-07-04 10:39:07 +0800

    Site updated: 2025-07-04 10:39:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["W5500使用/","Hexo/图床教程/","Hexo/图床教程1/","Hexo/图床教程2/"];function toRandomPost(){
+var posts=["W5500使用/","Hexo/图床教程1/","Hexo/图床教程2/","Hexo/图床教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
